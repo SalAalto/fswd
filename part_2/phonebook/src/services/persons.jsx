828de1f6d0e8a1c1fdb0a30d8bfc6327da25f1d8
SalAalto <salahuddin.salahuddin@aalto.fi>
2023-10-01 10:42:57 +0300

    initial version of phonebook complete

---
+++ part_2/phonebook/src/services/persons.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+const baseUrl = `https://phonebook-backend-as20203.fly.dev/api/persons`
+// const baseUrl = `http://localhost:3001/api/persons`
+
+const getAll = () => {
+    return axios.get(baseUrl)
+}
+
+const create = newObject => {
+    return axios.post(baseUrl, newObject)
+}
+
+const update = (id, newObject) => {
+    return axios.put(`${baseUrl}/${id}`, newObject)
+}
+
+const deletePerson = (id) => {
+return axios.delete(`${baseUrl}/${id}`)
+}
+
+const service = { 
+getAll: getAll, 
+create: create, 
+update: update,
+delete: deletePerson, 
+}
+export default  service
