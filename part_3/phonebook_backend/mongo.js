81fa09bb2024f0199a59dbb87d6d05cca535d8af
SalAalto <salahuddin.salahuddin@aalto.fi>
2023-10-21 19:42:23 +0300

    part 3 initial setup complete

--- part_3/phonebook_backend/mongo.js
+++ part_3/phonebook_backend/mongo.js
@@ -1,55 +1,62 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-require('dotenv').config()
-
-// if (process.argv.length < 3) {
-//     console.log('Please provide the password as an argument: node mongo.js <password>')
-//     process.exit(1)
-// }
-
-// const password = process.argv[2]
-const personName = process.argv?.[3]
-const phoneNumber = process.argv?.[4]
-// const url = `mongodb+srv://hsalahhuddin:${password}@cluster0.niapokj.mongodb.net/?retryWrites=true&w=majority`
-const url = process.env.MONGODB_URI
-const phonebookSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    number: String,
-})
-
-const Phonebook = mongoose.model('Phonebook', phonebookSchema)
-
-if (personName && personName && process.argv.length === 5) {
-    mongoose
-        .connect(url)
-        .then(async () => {
-            const person = new Phonebook({
-                number: phoneNumber,
-                name: personName
-            })
-            await person.save()
-            console.log(`added ${personName} number ${phoneNumber} to phonebook`)
-            return mongoose.connection.close()
-        })
-        .catch(error =>
-            console.log(error)
-        )
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>');
+    process.exit(1);
 }
 
-if (process.argv.length === 3) {
-    mongoose
-        .connect(url)
-        .then(async () => {
-            const persons = await Phonebook.find()
-            if (persons.length) {
-                console.log('phonebook:')
-                persons.forEach(({ name, number }) => console.log(`${name} ${number}`))
+const password = process.argv[2];
+const personName = process.argv[3];
+const phoneNumber = process.argv[4];
 
-            }
-            return mongoose.connection.close()
-        })
-        .catch(error =>
-            console.log(error)
-        )
+if (!password) {
+    console.log('Password is required.');
+    process.exit(1);
+}
+
+const url = `mongodb+srv://sala:${password}@cluster0.o4wnjoa.mongodb.net/?retryWrites=true&w=majority`;
+console.log(url);
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+const Phonebook = mongoose.model('Phonebook', phonebookSchema);
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+if (personName && phoneNumber && process.argv.length === 5) {
+    connectToDatabase().then(async () => {
+        const person = new Phonebook({
+            number: phoneNumber,
+            name: personName,
+        });
+
+        await person.save();
+        console.log(`added ${personName} number ${phoneNumber} to phonebook`);
+        mongoose.connection.close();
+    });
+} else if (process.argv.length === 3) {
+    connectToDatabase().then(async () => {
+        const persons = await Phonebook.find();
+
+        if (persons.length > 0) {
+            console.log('phonebook:');
+            persons.forEach(({ name, number }) => console.log(`${name} ${number}`));
+        }
+
+        mongoose.connection.close();
+    });
+} else {
+    console.log('Invalid command. Please provide valid arguments.');
+    process.exit(1);
 }
