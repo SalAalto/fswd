ad563eb67788990a6d739b996075cbdf74d94d7f
SalAalto <salahuddin.salahuddin@aalto.fi>
2023-10-22 11:07:58 +0300

    dist added back

--- part_2/phonebook/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: 'src/main.jsx', // Set the entry point to your JavaScript/TypeScript file
-    },
-  },
-});
